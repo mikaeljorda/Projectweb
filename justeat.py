641df0836881a9b1ecdd7572d8b798a153a169f5
Elmatracas <esmeralda.ortiz1779@alumnos.udg.mx>
2024-09-10 10:50:18 -0600

    p5 signup

--- justeat.py
+++ justeat.py
@@ -1,10 +1,14 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template, url_for
 
 justeat = Flask(__name__)
 
 @justeat.route('/')
 def home():
    return render_template('home.html')
+
+@justeat.route('/signup')
+def signup():
+  return render_template('signup.html')
 
 if __name__ == '__main__':
  justeat.run(debug=True,port=3300) 
